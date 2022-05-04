384ad8991db8761314697ecdd87e568556df4832
shpuravasiliy <shpura.vasiliy@gmail.com>
2022-05-04 21:44:36 +0300

    add a part of chapter 5

---
+++ myCode_chapter_5/dogObj.js
@@ -0,0 +1,16 @@
+var dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
+};
+var bark;
+if (dog.weight > 20) {
+    bark = "WOOF WOOF";
+} else {
+    bark = "woof woof";
+}
+var speak = dog.name + " says " + bark + " when he wants to " + dog.activity;
+console.log(speak);
+console.log(dog);
